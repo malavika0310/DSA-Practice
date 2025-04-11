46ba7a72a16d21ec937302d3c1d87427dad241f9
malavika0310 <malavikas0310@gmail.com>
2025-04-11 14:48:18 +0530

    Added a program to print a pattern - pattern3

---
+++ Loops/Pattern_Printing3.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    int stars=1;
+    int spaces=n-1;
+    for(int i=1;i<=2*n-1;i++){
+        for(int j=1;j<=spaces;j++){
+            cout<<" ";
+        }
+        for(int j=1;j<=stars;j++){
+            cout<<"*";
+        }
+        if(i<n){
+            stars+=2;
+            spaces--;
+        }else{
+            stars-=2;
+            spaces++;
+        }
+        cout<<endl;
+    }
+}
