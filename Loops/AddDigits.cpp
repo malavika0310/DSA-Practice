e18c7133a14a4b7645e1580c1cf9a18ea68eb3ba
malavika0310 <malavikas0310@gmail.com>
2025-04-12 13:11:34 +0530

    Added the program to add digits of a number repeatedly till the result has only one digit

---
+++ Loops/AddDigits.cpp
@@ -0,0 +1,19 @@
+// Problem: Add Digits
+// Link: https://leetcode.com/problems/add-digits/
+// Difficulty: Easy
+
+class Solution {
+    public:
+        int addDigits(int num) {
+            while(num>=10){
+                int sum=0;
+                while(num>0){
+                    sum+=num%10;
+                    num/=10;
+                }
+                num=sum;
+            }
+            return num;
+    
+        }
+    };
